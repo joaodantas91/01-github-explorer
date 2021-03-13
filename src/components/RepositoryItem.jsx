9460d9f178f016f446ac8c999dbda04e77e29a44
João Dantas <joaostadantas@gmail.com>
2021-03-12 21:00:55 -0300

    starting with typescript

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem({repository}) {
+  return (
+    <li>
+      <strong>{repository?.name}</strong>
+      <p>{repository?.description}</p>
+
+      <a href={repository?.html_url}>
+        Acessar repositório
+      </a>
+    </li>
+
+  );
+}
